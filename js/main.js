25ad7fd730b69b56ac4f8c07beeba3cae93906be
dcardenas09 <dcardenas09@gmail.com>
2015-12-17 18:47:22 -0500

    final product

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+        var op1;
+        var sign;
+        var op2;
     $(".button, #zero").on("click", function(){
         var valueClicked = $(this).attr("tech");
         if($("#screen").html().length < 12){ 
@@ -10,13 +13,20 @@ $(document).ready(function() {
         var extraClicks = $(this).attr("zone");
         console.log(extraClicks);
     });
-
     
+    $("#equals").click(function(){
+        op2 = $("#screen").html();
+        $("#screen").html(eval( op1 + sign + op2 ) );
+    });
+    
+    $(".operators").click(function(){
+        sign = $(this).html();
+        op1 = $("#screen").html();
+        $("#screen").html("");
+    });
+
     $("#clear-all").click(function() {
         $("#screen").html("");
     });
 });
-
-
-
   
